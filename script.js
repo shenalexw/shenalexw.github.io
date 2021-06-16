21c817308832ce5f68c0d6a2aeb0769b10802aef
Alex Shen <shenalexw@gmail.com>
2021-06-15 21:06:17 -0700

    Navigation bar is responsive

---
+++ script.js
@@ -0,0 +1,16 @@
+function navSlide() {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+    
+    burger.addEventListener("click", () => {
+        //Toggle Nav
+        nav.classList.toggle("nav-active");
+        
+        //Burger Animation
+        burger.classList.toggle("toggle");
+    });
+    
+}
+
+navSlide();
