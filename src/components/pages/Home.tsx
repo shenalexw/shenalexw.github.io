2217d7ab638ef8b5c986a8a092d971c283dba572
Alexander Shen <shenalexw@gmail.com>
2022-12-16 16:02:23 -0800

    fix home nav and update resume

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -18,7 +18,7 @@ export default class Home extends Component<Props, State> {
                         <div className='home-button-row'>
                             <button onClick={() => window.open("https://www.linkedin.com/in/shenalexw/")}>LinkedIn</button>
                             <button onClick={() => window.open("https://github.com/shenalexw")}>github</button>
-                            <button onClick={() => window.open("https://drive.google.com/file/d/1krDvLJZWY2w5JQssSARQH457Q4UfFJBN/view?usp=sharing")}>Resumé</button>
+                            <button onClick={() => window.open("https://drive.google.com/file/d/1hYl_6yNotL1W-4MPfkC7wW7k0r7IOUVY/view?usp=sharing")}>Resumé</button>
                         </div>
                     </div>
                 </div>
